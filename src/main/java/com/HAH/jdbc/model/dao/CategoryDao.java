37b866dc78fd11b46fcee04115880e44559ae3a7
hhtun137 <htunaunghlaing199@gmail.com>
2024-03-18 15:03:55 +0630

    using test cases & categoryDao for jdbcTemplate

--- src/main/java/com/HAH/jdbc/model/dao/CategoryDao.java
+++ src/main/java/com/HAH/jdbc/model/dao/CategoryDao.java
@@ -1,12 +1,13 @@
 package com.HAH.jdbc.model.dao;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.jdbc.core.JdbcOperations;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Service;
 import com.HAH.jdbc.model.dto.Category;
@@ -15,16 +16,55 @@ import com.HAH.jdbc.model.dto.Category;
 public class CategoryDao {
 
 	@Autowired
-	private JdbcOperations jdbc;
-
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
-
-	@Autowired
 	private SimpleJdbcInsert insert;
 
-	@Value("${dml.category.insert}")
+	@Value("${dao.category.insert}")
 	private String insertSql;
+	@Value("${dao.category.update}")
+	private String update;
+	@Value("${dao.category.findById}")
+	private String findById;
+	@Value("${dao.category.findByNameLike}")
+	private String findByNameLike;
+	@Value("${dao.category.findCountByNameLike}")
+	private String findCountByNameLike;
+	@Value("${dao.category.delete}")
+	private String delete;
+
+	private BeanPropertyRowMapper<Category> rowMapper;
+
+	public CategoryDao() {
+		rowMapper = new BeanPropertyRowMapper<>(Category.class);
+	}
+
+	public int create(Category c) {
+
+		Map<String, Object> param = new HashMap<>();
+		param.put("name", c.getName());
+
+		return insert.executeAndReturnKey(param).intValue();
+	}
+
+	public int update(Category c) {
+		return insert.getJdbcTemplate().update(update, c.getName(), c.getId());
+	}
+
+	public Category findById(int id) {
+		return insert.getJdbcTemplate().queryForObject(findById, rowMapper, id);
+	}
+
+	public List<Category> findByNameLike(String name) {
+		return insert.getJdbcTemplate().query(findByNameLike, rowMapper, name.toLowerCase().concat("%"));
+	}
+
+	public int findCountByNameLike(String name) {
+		return insert.getJdbcTemplate().queryForObject(findCountByNameLike, Integer.class,
+				name.toLowerCase().concat("%"));
+	}
+
+	public int delete(int id) {
+		return insert.getJdbcTemplate().update(delete, id);
+	}
 
 //	public int create(Category c) {
 //
@@ -36,13 +76,5 @@ public class CategoryDao {
 //		jdbc.update(creator, keyholder);
 //		return keyholder.getKey().intValue();
 //	}
-
-	public int create(Category c) {
-
-		Map<String, Object> param = new HashMap<>();
-		param.put("name", c.getName());
-
-		return insert.executeAndReturnKey(param).intValue();
-	}
 
 }
--- src/test/java/com/HAH/jdbc/model/test/JdbcTest.java
+++ src/test/java/com/HAH/jdbc/model/test/JdbcTest.java
@@ -2,6 +2,10 @@ package com.HAH.jdbc.model.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
@@ -20,11 +24,63 @@ public class JdbcTest {
 	private CategoryDao categoryDao;
 
 	@Test
-	void TestDemo() {
+	@DisplayName("1. Create Category")
+	@Order(1)
+	void test1() {
 		var c = new Category();
 		c.setName("Foods");
 
 		var id = categoryDao.create(c);
+		System.out.println(c.getName());
 		assertEquals(1, id);
+	}
+
+	@Test
+	@DisplayName("2. Update Category")
+	@Order(2)
+	void test2() {
+		var c = new Category();
+		c.setId(1);
+		c.setName("Drinks");
+
+		int count = categoryDao.update(c);
+		System.out.println(c.getName());
+		assertEquals(1, count);
+	}
+
+	@Test
+	@DisplayName("3. Find Category By Id")
+	@Order(3)
+	void test3() {
+		Category c = categoryDao.findById(1);
+		System.out.println(c);
+		assertEquals("Drinks", c.getName());
+	}
+
+	@Test
+	@DisplayName("4. Find Category By Name Like")
+	@Order(4)
+	void test4() {
+		List<Category> list = categoryDao.findByNameLike("Drin");
+		System.out.println(list);
+		assertEquals(1, list.size());
+	}
+
+	@Test
+	@DisplayName("5. Find Count By Name Like")
+	@Order(5)
+	void test5() {
+		int count = categoryDao.findCountByNameLike("Drin");
+		System.out.println(count);
+		assertEquals(1, count);
+	}
+
+	@Test
+	@DisplayName("6. Find Count By Name Like")
+	@Order(6)
+	void test6() {
+		int count = categoryDao.delete(1);
+		System.out.println(count);
+		assertEquals(1, count);
 	}
 }
